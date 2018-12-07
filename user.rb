85b97cc19b5fb976e65e3f49c1e7ec0b8e95b6b5
Stephany Lopez <stephany.lopez1@ymail.com>
2018-12-06 21:02:05 -0600

    Updating views

--- user.rb
+++ user.rb
@@ -1,4 +1,4 @@
-require 'data_mapper' # metagem, requires common plugins too.
+require 'data_mapper'
 require_relative 'listing.rb'
 require 'stripe'
 
@@ -7,8 +7,6 @@ set :secret_key, "sk_test_aTWBdKlOMVvvoJtaz7xjfFIg"
 
 Stripe.api_key = settings.secret_key
 
-
-require_relative "listing.rb"
 
 if ENV['DATABASE_URL']
   DataMapper::setup(:default, ENV['DATABASE_URL'] || 'postgres://localhost/mydb')
@@ -22,8 +20,7 @@ class User
     property :email, String
     property :password, String
     property :skills, Text
-    property :first_name, Text
-    property :last_name, Text
+    property :full_name, Text
     property :preferred_location, Text
     property :pro, Boolean, :default => false
     property :administrator, Boolean, :default => false
@@ -42,6 +39,7 @@ Listing.auto_upgrade!
 #make an admin user if one doesn't exist!
 if User.all(administrator: true).count == 0
 	u = User.new
+   u.full_name = "Admin"
 	u.email = "admin@admin.com"
 	u.password = "admin"
 	u.administrator = true
@@ -68,7 +66,7 @@ post "/charge" do
     :currency    => 'usd',
     :customer    => customer.id
   )
-  erb :charge
+  erb :account_profile
 end
 
 
@@ -88,21 +86,20 @@ get '/account/edit_profile' do
    erb :edit_profile
 end
 
-# updates user account with name, skills, and preferred location
+# Updates user account with name, skills, and preferred location
 post '/update_profile' do
-   if params["first_name"] && params["last_name"] && params["skills"]
-      current_user.first_name = params["first_name"]
-      current_user.last_name = params["last_name"]
+   if params["full_name"] && params["skills"]
+      current_user.full_name = params["full_name"]
       current_user.skills = params["skills"]
       current_user.preferred_location = params["preferred_location"]
       current_user.save
-      erb :account_profile
    else
-      return "Error! You're missing a parameter. "
+      return "Error: you're missing a parameter!"
    end
 end
 
-# displays current profile
+
+# GET request that displays a pro user's current profile
 get '/profile' do
    authenticate!
    pro_only!

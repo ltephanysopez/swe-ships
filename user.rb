de6911e15d400db2ee94db104b5cdc71c1d3d60c
Stephany <33434208+ltephanysopez@users.noreply.github.com>
2018-12-12 17:37:37 -0600

    Updating API keys

--- user.rb
+++ user.rb
@@ -2,8 +2,8 @@ require 'data_mapper'
 require 'stripe'
 require_relative 'listing.rb'
 
-set :publishable_key, "pk_test_Y19q4uOMtZL9rfgh3uHTtEo5"
-set :secret_key, "sk_test_aTWBdKlOMVvvoJtaz7xjfFIg"
+set :publishable_key, ENV['PUBLISHABLE_KEY']
+set :secret_key, ENV['SECRET_KEY']
 
 Stripe.api_key = settings.secret_key
 
